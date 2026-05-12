2a2226a4966a2230042fcd06b63ceb4685a287f7
abdomagdy1012 <62938057+abdomagdy1012@users.noreply.github.com>
2026-05-12 15:56:03 +0300

    fix timer initialization
    Fixed initial timer value and added functionality to handle game end conditions.

--- project javascript/function.js
+++ project javascript/function.js
@@ -1,190 +1,208 @@
-
 let loc = location.search.split("&")[1];
 let locat = loc.split("=")[1];
+
 let btn = document.querySelector(".button");
 let div = document.querySelector(".container");
 let body = document.querySelector("body");
+
 let timer = document.querySelector(".Timer");
 let score = document.querySelector("#score");
-let scoreTotal = document.querySelector("body > div.containers > div > span")
-let winscore = document.querySelector("body > div.popup > span")
 let kill = document.querySelector(".kill");
+
 let birdos = document.querySelector("#birds");
 let bomb = document.querySelector(".bomb");
-let po = document.getElementById("po"); // enter name
+
+let po = document.getElementById("po");
 let names = document.querySelector(".name");
+
 let containers = document.querySelector(".containers");
-let pop =document.querySelector("body > div.popup")
-let btn2 = document.querySelector("body > div.containers > div > button")
-let btn3 = document.querySelector("body > div.popup > button")
+let pop = document.querySelector(".popup");
+
+let btn2 = document.querySelector("body > div.containers > div > button");
+let btn3 = document.querySelector("body > div.popup > button");
+
+let scoreTotal = document.querySelector("body > div.containers > div > span");
+let winscore = document.querySelector("body > div.popup > span");
 
 let x = 0;
-score.innerText += x;
 let k = 0;
-kill.innerText +=k;
-let min = 10;
+
+let min = 60; // FIXED start time
+
+let ids; // timer interval
+let birdInterval;
+let bombInterval;
+
+score.innerText = `score: ${x}`;
+kill.innerText = `Birds Killed: ${k}`;
+
 po.innerHTML += locat;
 names.innerHTML += locat;
 
+window.onload = function () {
 
+    btn2.onclick = () => location.reload();
+    btn3.onclick = () => location.reload();
 
-window.onload = function(){
+    btn.onclick = function () {
 
+        div.style.display = "none";
 
-    btn2.onclick = function(){
-     location.reload();
-    }
-    btn3.onclick = function(){
-        location.reload();
-       }
-btn.onclick = function(){    //remove popup
-    div.style.display = "none";
+        // ================= TIMER =================
+        function count() {
+            if (min > 0) {
+                min--;
+                timer.innerText = `Time Limit ${min}`;
+            } else {
+                endGame();
+            }
+        }
 
-    const generateBirds =function(){ 
-    let id1=  setInterval (function(){
-        
-    let ranBird = Math.floor(((Math.random()*3)+1));
-    let bird = document.createElement("img");
-    bird.src =`images/${ranBird}.gif`;
-    bird.classList.add("bird");
-    birdos.appendChild(bird);
-    let height = Math.round(Math.random()*(window.innerHeight - bird.height-50));
-    bird.style.top = height +"px";
-    bird.style.left = 0;
+        ids = setInterval(count, 1000);
 
-    let left = 0;
-    let id = setInterval(function(){
-        left+=10;
-        bird.style.left = left+"px";
-                if(left >window.innerWidth + bird.width){
-                
-                    clearInterval(id);
-                    body.style.overflow = "hidden";
+        // ================= END GAME =================
+        function endGame() {
+
+            clearInterval(ids);
+            clearInterval(birdInterval);
+            clearInterval(bombInterval);
+
+            let finalScore = x;
+
+            localStorage.setItem("name", po.innerText);
+            localStorage.setItem("score", finalScore);
+
+            if (finalScore > 50) {
+                pop.style.display = "block";
+                winscore.innerText = `Your final score: ${finalScore}`;
+            } else {
+                containers.style.display = "block";
+                scoreTotal.innerText = `Your final score: ${finalScore}`;
+            }
+        }
+
+        // ================= BIRDS =================
+        const generateBirds = function () {
+
+            birdInterval = setInterval(function () {
+
+                let ranBird = Math.floor(Math.random() * 3 + 1);
+
+                let bird = document.createElement("img");
+                bird.src = `images/${ranBird}.gif`;
+                bird.classList.add("bird");
+                birdos.appendChild(bird);
+
+                let height = Math.random() * (window.innerHeight - 100);
+                bird.style.top = height + "px";
+                bird.style.left = "0px";
+
+                let left = 0;
+
+                let move = setInterval(function () {
+                    left += 10;
+                    bird.style.left = left + "px";
+
+                    if (left > window.innerWidth) {
+                        clearInterval(move);
+                        bird.remove();
+                    }
+                }, 30);
+
+                bird.onclick = function () {
+
+                    if (min <= 0) return;
+
+                    if (bird.src.includes("3.gif")) {
+                        x += 5;
+                    } else if (bird.src.includes("1.gif")) {
+                        x += 10;
+                    } else {
+                        x -= 10;
+                    }
+
+                    k++;
+
+                    score.innerText = `score: ${x}`;
+                    kill.innerText = `Birds Killed: ${k}`;
+
                     bird.remove();
-                    
-                    
-                } 
-                
-            },30)
+                };
 
+            }, 600);
+        };
 
-        bird.onclick = function(){
-            if(bird.src =="http://127.0.0.1:5500/images/3.gif"){
-            bird.remove();
-            score.innerText = `score: ${x += 5}`;
-            kill.innerText = `Birds Killed: ${k++}`;
-        } else if(bird.src =="http://127.0.0.1:5500/images/1.gif"){
-            bird.remove();
-            score.innerText = `score: ${x += 10}`
-            kill.innerText = `Birds Killed ${k++}`;
-        } else{
-            bird.remove();
-            score.innerText = `score: ${x -= 10}`;
-            kill.innerText = `Birds Killed: ${k++}`;
-        }
-    
-        }
+        // ================= BOMBS =================
+        const generateBomb = function () {
 
-        if(min == "0"){
-            clearInterval(id1);
-            
-           
-        }
-        
-        
-},600)
+            bombInterval = setInterval(function () {
 
-}  // nd of generate
+                let bombs = document.createElement("img");
+                bombs.src = `images/bomb/1.png`;
+                bombs.classList.add("bombImg");
 
-function count(){
-    if(min > 0){
-        min -= 1;
-        timer.innerText = `Time Limit ${min}`;
-    }
-    else{
+                bomb.appendChild(bombs);
 
-    if(parseInt(score.innerText.split(":")[1] )> 50){
-    
-    pop.style.display = "block";
-    winscore.innerText = `Your final ${score.innerText}`;   
-    clearInterval(ids);  
-    }
-    else{
-    containers.style.display = "block";
-    scoreTotal.innerText = `Your final ${score.innerText}`;
-    clearInterval(ids);
-    }
-    localStorage.setItem("name",po.innerText);
-    localStorage.setItem("score",score.innerText);
+                let width = Math.random() * (window.innerWidth - 50);
+                bombs.style.left = width + "px";
+                bombs.style.top = "0px";
 
-}
-}
-let ids= setInterval(count,1000); // end of count
+                let top = 0;
 
+                let fall = setInterval(function () {
+                    top += 10;
+                    bombs.style.top = top + "px";
 
-const generateBomb = function(){
-     let id1  = setInterval (function(){
-        
-        let ranbomb = Math.floor(((Math.random()*1)+1));
-        let bombs = document.createElement("img");
-        bombs.src =`images/bomb/${ranbomb}.png`;
-        bomb.appendChild(bombs);
-        let width = Math.round(Math.random()*(window.innerWidth - bombs.width-50));
-        bombs.style.left = width +"px";
-        bombs.style.top = 0;
-
-        let top = -10;
-        let id = setInterval(function(){
-            top+=10;
-            bombs.style.top = top+"px";
-                    if(top >window.innerHeight+ bombs.width){
-                    
-                        clearInterval(id);
+                    if (top > window.innerHeight) {
+                        clearInterval(fall);
                         bombs.remove();
-                        body.style.overflow = "hidden";
-                    } 
-                    
-                },100)
-                bombs.onclick = function(){
-                    let arr = document.querySelectorAll(".bird");
-                    arr.forEach(bird => {
-                        let  birdX = parseInt( bird.style.left);
-                        let birdY = parseInt( bird.style.top);
-                        let bombX = parseInt(bombs.style.left);
-                        let bombY = parseInt(bombs.style.top);
-                        let distance = parseInt(Math.sqrt(Math.pow((birdX - bombX),2) + Math.pow((birdY - bombY),2)))
-                        if(distance < 300){
-                            if(bird.src =="http://127.0.0.1:5500/images/3.gif"){
-                                bird.remove();
-                                score.innerText = `score: ${x += 5}`;
-                                kill.innerText = `Birds Killed: ${k++}`;
-                            } else if(bird.src =="http://127.0.0.1:5500/images/1.gif"){
-                                bird.remove();
-                                score.innerText = `score: ${x += 10}`
-                                kill.innerText = `Birds Killed ${k++}`;
-                            } else{
-                                bird.remove();
-                                score.innerText = `score: ${x -= 10}`;
-                                kill.innerText = `Birds Killed: ${k++}`;
+                    }
+
+                }, 100);
+
+                bombs.onclick = function () {
+
+                    let birds = document.querySelectorAll(".bird");
+
+                    birds.forEach(bird => {
+
+                        let bx = parseInt(bird.style.left);
+                        let by = parseInt(bird.style.top);
+
+                        let ox = parseInt(bombs.style.left);
+                        let oy = parseInt(bombs.style.top);
+
+                        let distance = Math.sqrt(
+                            Math.pow(bx - ox, 2) + Math.pow(by - oy, 2)
+                        );
+
+                        if (distance < 300) {
+
+                            if (bird.src.includes("3.gif")) {
+                                x += 5;
+                            } else if (bird.src.includes("1.gif")) {
+                                x += 10;
+                            } else {
+                                x -= 10;
                             }
+
+                            k++;
+
+                            score.innerText = `score: ${x}`;
+                            kill.innerText = `Birds Killed: ${k}`;
+
+                            bird.remove();
                         }
-                        
                     });
-                    
+
                     bombs.remove();
-                }
-                if(min == "0"){
-                    clearInterval(id1);
-                }
-    },2000)
-}
+                };
 
+            }, 2000);
+        };
 
-
-generateBirds();
-generateBomb();
-
-}
-
-
-}
+        // START GAME
+        generateBirds();
+        generateBomb();
+    };
+};
